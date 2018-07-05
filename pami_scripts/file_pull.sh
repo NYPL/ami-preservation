f7d1770d3afb6d83e2adab9057394cbbc2642f50
bturkus <benjaminturkus@nypl.org>
2018-07-05 11:16:16 -0400

    small tweak for wildcard division code

--- pami_scripts/file_pull.sh
+++ pami_scripts/file_pull.sh
@@ -2,8 +2,8 @@
 
 IFS=$'\n' read -d '' -r -a files < $1
 
-for i in "${files[@]}" ; do find ./ $2 -name $i*sc.mp4 -exec rsync -rtv --progress {} $3 ';' ; done
-for i in "${files[@]}" ; do find ./ $2 -name $i*em.wav -exec rsync -rtv --progress {} $3 ';' ; done
+for i in "${files[@]}" ; do find ./ $2 -name *$i*sc.mp4 -exec rsync -rtv --progress {} $3 ';' ; done
+for i in "${files[@]}" ; do find ./ $2 -name *$i*em.wav -exec rsync -rtv --progress {} $3 ';' ; done
 
 cd $3
 for file in *wav ; do ffmpeg -i "$file" -c:a aac -b:a 192k -dither_method rectangular -ar 44100 "${file%.*}.mp4" ; done
