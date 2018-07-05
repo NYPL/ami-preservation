2d90e6f89e7b4bd6504a526e7f3ba5f22d1f558f
bturkus <benjaminturkus@nypl.org>
2018-07-05 10:33:55 -0400

    add file pull
    for automating collection-level file pulls

---
+++ pami_scripts/file_pull.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IFS=$'\n' read -d '' -r -a files < $1
+
+for i in "${files[@]}" ; do find ./ $2 -name $i*sc.mp4 -exec rsync -rtv --progress {} $3 ';' ; done
+for i in "${files[@]}" ; do find ./ $2 -name $i*em.wav -exec rsync -rtv --progress {} $3 ';' ; done
+
+cd $3
+for file in *wav ; do ffmpeg -i "$file" -c:a aac -b:a 192k -dither_method rectangular -ar 44100 "${file%.*}.mp4" ; done
+for file in *wav ; do rm $file ; done
+
+echo ""
+echo "========================================================="
+echo "====================Summary Stats========================"
+echo ""
+echo "Total Files Pulled:" $(ls | wc -l)
+echo ""
+ls
