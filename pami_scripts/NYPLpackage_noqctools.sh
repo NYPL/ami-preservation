0de51302a8de3da67f81356c22955a0779ffea19
bturkus <bturkus@gmail.com>
2018-12-20 13:50:22 -0500

    uno mas change transcode audio bitrate

--- pami_scripts/NYPLpackage_noqctools.sh
+++ pami_scripts/NYPLpackage_noqctools.sh
@@ -21,7 +21,7 @@ mv *.log AuxiliaryFiles
 
 for file in *.mov ; do
   ffmpeg -i "$file" -map 0 -dn -c:v ffv1 -level 3 -g 1 -slicecrc 1 -slices 24 -field_order bt -vf setfield=bff,setdar=4/3 -color_primaries smpte170m -color_range tv -color_trc bt709 -colorspace smpte170m -c:a copy "${file%.*}.mkv" ;
-  ffmpeg -i "$file" -map 0 -dn -c:v libx264 -movflags faststart -pix_fmt yuv420p -b:v 3500000 -bufsize 1750000 -maxrate 3500000 -vf yadif -c:a aac -strict -2 -b:a 192000 -ar 48000 -y -s 720x486 -aspect 4:3 "${file%.*}_sc.mp4"
+  ffmpeg -i "$file" -map 0 -dn -c:v libx264 -movflags faststart -pix_fmt yuv420p -b:v 3500000 -bufsize 1750000 -maxrate 3500000 -vf yadif -c:a aac -strict -2 -b:a 320000 -ar 48000 -y -s 720x486 -aspect 4:3 "${file%.*}_sc.mp4"
 done
 for file in *.mkv ; do
   ffmpeg -i "$file" -f framemd5 -an "${file%*}.framemd5" ;
