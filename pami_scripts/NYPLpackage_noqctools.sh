e2558ad81cd2b5548ae8d741e8c1e4f2733f96be
Ben Turkus <bturkus@gmail.com>
2018-12-20 13:38:52 -0500

    update NYPLpackage w/ dv

--- pami_scripts/NYPLpackage_noqctools.sh
+++ pami_scripts/NYPLpackage_noqctools.sh
@@ -7,12 +7,16 @@ for file in *.mkv ; do
   ffmpeg -i "$file" -map 0:v -map 0:a -c:v libx264 -movflags faststart -pix_fmt yuv420p -b:v 3500000 -bufsize 1750000 -maxrate 3500000 -vf yadif -c:a aac -strict -2 -b:a 192000 -ar 48000 -y -s 720x486 -aspect 4:3 "${file%.*}_sc.mp4" ;
 done
 
+for file in *.dv ; do
+  ffmpeg -i "$file" -map 0:v -map 0:a -c:v libx264 -movflags faststart -pix_fmt yuv420p -b:v 3500000 -bufsize 1750000 -maxrate 3500000 -vf yadif -c:a aac -strict -2 -b:a 192000 -ar 48000 -y -s 720x486 -aspect 4:3 "${file%.*}_sc.mp4" ;
+done
+
 mkdir PreservationMasters
 mkdir ServiceCopies
 mkdir V210
 mkdir AuxiliaryFiles
 
-mv *.mkv *.framemd5 *.xml.gz PreservationMasters
+mv *.mkv *.framemd5 *.xml.gz *.dv PreservationMasters
 mv *.log AuxiliaryFiles
 
 for file in *.mov ; do
