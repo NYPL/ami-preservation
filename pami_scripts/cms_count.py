be738aedc7d050f02af49e3aef238fa9fc641c69
bturkus <bturkus@gmail.com>
2018-12-20 13:46:50 -0500

    update cmscount

--- pami_scripts/cms_count.py
+++ pami_scripts/cms_count.py
@@ -10,7 +10,7 @@ file_set = set()
 
 for root, dirs, files in os.walk(root_dir):
   for file in files:
-      if file.endswith('.mp4'):
+      if file.endswith(('.mov', '.wav')):
           file_set.add(file)
           cms = file.split('_')[1]
           cms_set.add(cms)
@@ -18,5 +18,5 @@ for root, dirs, files in os.walk(root_dir):
 print(len(file_set))
 print(len(cms_set))
 
-for item in cms_set:
+for item in sorted(list(cms_set)):
     print(item)
