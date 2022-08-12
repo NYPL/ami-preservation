3da46a8ea93f6261fd17e8de0b75921ba0173364
Ryan Marino <40605190+rdmarino@users.noreply.github.com>
2022-08-12 10:58:28 -0400

    Update mediaconch_video_inHouse_SC.sh

--- qc_utilities/mediaconch_video_inHouse_SC.sh
+++ qc_utilities/mediaconch_video_inHouse_SC.sh
@@ -16,6 +16,6 @@ log_path="$log_dir/mediaconch_videoSC_$dateCreated.csv"
 i=0
 
 for line in $dir_of_bags; do
-find Video/ -name "*.mp4" -exec mediaconch -p /$log_dir/ami-preservation/qc_utilities/MediaconchPolicies/MediaConch_NYPL_video_SC.xml {} ';' > $log_path
+find $PWD/ -name "*.mp4" -exec mediaconch -p /$log_dir/ami-preservation/qc_utilities/MediaconchPolicies/MediaConch_NYPL_video_SC.xml {} ';' > $log_path
 
 done
