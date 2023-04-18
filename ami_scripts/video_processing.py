0c5c033eb208391570f0c8a624824b1fb555d117
bturkus <benjaminturkus@nypl.org>
2023-04-18 12:48:23 -0400

    add mediainfo extraction to video processing

--- ami_scripts/video_processing.py
+++ ami_scripts/video_processing.py
@@ -6,7 +6,14 @@ import os
 import shutil
 import pathlib
 import itertools
+import csv
+import re
+import logging
+from pymediainfo import MediaInfo
 
+LOGGER = logging.getLogger(__name__)
+video_extensions = {'.mkv', '.mov', '.mp4', '.dv', '.iso'}
+audio_extensions = {'.wav', '.flac'}
 
 def rename_files(input_directory, extensions):
     files = set(itertools.chain.from_iterable(input_directory.glob(ext) for ext in extensions))
@@ -122,38 +129,129 @@ def delete_empty_directories(input_directory, directories):
         except FileNotFoundError:
             pass
 
+def process_directory(directory):
+    valid_extensions = video_extensions.union(audio_extensions)
+    return [path for path in directory.rglob('*') if path.is_file() and path.suffix.lower() in valid_extensions]
 
-def main(input_directory, transcribe):
-    extensions = ("*.mov", "*.mkv", "*.xml.gz", "*.framemd5")
-    rename_files(input_directory, extensions)
-    convert_mkv_dv_to_mp4(input_directory)
-        
-    process_mov_files(input_directory)
-    generate_framemd5_files(input_directory)
-
-    if transcribe:
-        transcribe_files(input_directory)
-    
-    directories = ["PreservationMasters", "ServiceCopies", "V210", "AuxiliaryFiles"]
-    create_directories(input_directory, directories)
-
-    move_files(input_directory)
-    move_log_files_to_auxiliary_files(input_directory)
-    
-    delete_empty_directories(input_directory, ["AuxiliaryFiles", "V210"])
+def has_mezzanines(file_path):
+    for parent in file_path.parents:
+        mezzanines_dir = parent / "Mezzanines"
+        if mezzanines_dir.is_dir():
+            return True
+    return False
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process video files in a specified directory.")
+def extract_track_info(media_info, path, project_code_pattern, valid_extensions):
+    for track in media_info.tracks:
+        if track.track_type == "General":
+            file_data = [
+                path,
+                '.'.join([path.stem, path.suffix[1:]]),
+                path.stem,
+                path.suffix[1:],
+                track.file_size,
+                track.file_last_modification_date.split()[1],
+                track.format,
+                track.audio_format_list.split()[0] if track.audio_format_list else None,
+                track.codecs_video,
+                track.duration,
+            ]
+
+            if track.duration:
+                human_duration = str(track.other_duration[3]) if track.other_duration else None
+                file_data.append(human_duration)
+            else:
+                file_data.append(None)
+
+            media_type = None
+            has_mezzanines_folder = has_mezzanines(path)
+
+            if path.suffix.lower() in video_extensions:
+                media_type = 'film' if has_mezzanines_folder else 'video'
+            elif path.suffix.lower() in audio_extensions:
+                media_type = 'audio'
+
+            file_data.append(media_type)
+            file_no_ext = path.stem
+            role = file_no_ext.split('_')[-1]
+            division = file_no_ext.split('_')[0]
+            driveID = path.parts[2]
+            file_data.extend([role, division, driveID])
+            primaryID = path.stem
+            file_data.append(primaryID.split('_')[1] if len(primaryID.split('_')) > 1 else None)
+
+            match = project_code_pattern.search(str(path))
+            if match:
+                projectcode = match.group(1)
+                file_data.append(projectcode)
+            else:
+                file_data.append(None)
+
+            return file_data
+
+    return None
+
+def main():
+    parser = argparse.ArgumentParser(description="Process video files in a specified directory and optionally extract MediaInfo.")
     parser.add_argument("-d", "--directory", type=str, required=True, help="Input directory containing video files.")
     parser.add_argument("-t", "--transcribe", action="store_true", help="Transcribe the audio of the MKV files to VTT format using the Whisper tool.")
-    
+    parser.add_argument("-o", "--output", help="Path to save csv (optional). If provided, MediaInfo extraction will be performed.", required=False)
     args = parser.parse_args()
-    
+
     input_dir = pathlib.Path(args.directory)
-    
+
     if not input_dir.is_dir():
         print(f"Error: {input_dir} is not a valid directory.")
         exit(1)
-    
-    main(input_dir, args.transcribe)
+
+    create_directories(input_dir, ["AuxiliaryFiles", "V210", "PreservationMasters", "ServiceCopies"])
+    convert_mkv_dv_to_mp4(input_dir)
+    process_mov_files(input_dir)
+    generate_framemd5_files(input_dir)
+    rename_files(input_dir, video_extensions.union(audio_extensions))
+    move_files(input_dir)
+    move_log_files_to_auxiliary_files(input_dir)
+
+    if args.transcribe:
+        transcribe_files(input_dir)
+
+    delete_empty_directories(input_dir, ["AuxiliaryFiles", "V210", "PreservationMasters", "ServiceCopies"])
+
+    if args.output:
+        project_code_pattern = re.compile(r'(\d{4}_\d{2}_\d{2})')
+        valid_extensions = video_extensions.union(audio_extensions)
+        file_data = []
+
+        for path in process_directory(input_dir):
+            media_info = MediaInfo.parse(str(path))
+            track_info = extract_track_info(media_info, path, project_code_pattern, valid_extensions)
+            if track_info:
+                print(file_data)
+                file_data.append(track_info)
+
+        with open(args.output, "w", newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow([
+            'filePath',
+            'asset.referenceFilename',
+            'technical.filename',
+            'technical.extension',
+            'technical.fileSize.measure',
+            'technical.dateCreated',
+            'technical.fileFormat',
+            'technical.audioCodec',
+            'technical.videoCodec',
+            'technical.durationMilli.measure',
+            'technical.durationHuman',
+            'mediaType',
+            'role',
+            'divisionCode',
+            'driveID',
+            'primaryID',
+            'projectID'
+        ])
+            csvwriter.writerows(file_data)
+
+if __name__ == "__main__":
+    main()
+
