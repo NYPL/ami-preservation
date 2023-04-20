31b6a3a590f7d60a779b6b94616beb74e46fc716
bturkus <benjaminturkus@nypl.org>
2023-04-20 16:05:52 -0400

    make unbag work properly

--- ami_scripts/unbag_objects.py
+++ ami_scripts/unbag_objects.py
@@ -3,6 +3,7 @@
 import argparse
 from pathlib import Path
 import re
+import shutil
 
 def get_args():
     """Parse command line arguments."""
@@ -12,6 +13,7 @@ def get_args():
     args = parser.parse_args()
     return args
 
+
 def get_directory(args):
     """Get source directory from arguments."""
     source_directory = Path(args.source)
@@ -20,50 +22,67 @@ def get_directory(args):
 
     return source_directory
 
+
 def move_files_to_subfolders(source_directory):
     """Move files to their respective subfolders based on their file extensions."""
 
-    pm_path = source_directory / 'PreservationMasters'
-    sc_path = source_directory / 'ServiceCopies'
-    em_path = source_directory / 'EditMasters'
-
-    pm_path.mkdir(exist_ok=True)
-    sc_path.mkdir(exist_ok=True)
-    em_path.mkdir(exist_ok=True)
-
     file_mappings = [
-        (('.mkv', 'pm.json', '.dv', '.framemd5', '.gz', 'graphs.jpeg', 'timecodes.txt', 'pm.wav', 'pm.flac', '.iso', 'pm.cue'), pm_path),
-        (('.mp4', 'sc.json'), sc_path),
-        (('em.wav', 'em.json', 'em.flac'), em_path),
+        (('.mkv', 'pm.json', '.dv', '.framemd5', '.gz', 'graphs.jpeg', 'timecodes.txt', 'pm.wav', 'pm.flac', '.iso', 'pm.cue'), 'PreservationMasters'),
+        (('.mp4', 'sc.json'), 'ServiceCopies'),
+        (('em.wav', 'em.json', 'em.flac'), 'EditMasters'),
     ]
+
+    created_folders = {}
 
     for filepath in source_directory.glob('**/*'):
         if filepath.is_file():
-            for extensions, dest_folder in file_mappings:
-                if filepath.suffix in extensions:
+            for extensions, folder_name in file_mappings:
+                if filepath.suffix[1:] in extensions or any(name in filepath.name for name in extensions):
+                    dest_folder = source_directory / folder_name
+
+                    # Create folder only if not already created
+                    if folder_name not in created_folders:
+                        dest_folder.mkdir(exist_ok=True)
+                        created_folders[folder_name] = dest_folder
+
                     print(f'Moving: {filepath.name}')
                     try:
-                        filepath.rename(dest_folder / filepath.name)
+                        shutil.move(str(filepath), str(dest_folder / filepath.name))
                     except Exception as e:
                         print(f'Error moving file {filepath.name}: {e}')
                     break
 
-def clean_up(source_directory):
-    """Remove empty directories."""
+    return file_mappings
+
+
+def clean_up(source_directory, file_mappings):
+    """Remove empty directories and directories not containing files matching file_mappings."""
     for directory in source_directory.iterdir():
         cms = re.search(r'(\d{6})', directory.name)
         if cms and directory.is_dir():
-            if not any(directory.iterdir()):  # Check if the directory is empty
-                print(f'Deleting empty directory: {directory.name}')
-                directory.rmdir()
+            valid_files = False
+            for filepath in directory.glob('*'):
+                if filepath.is_file():
+                    for extensions, folder_name in file_mappings:
+                        if filepath.suffix[1:] in extensions or any(name in filepath.name for name in extensions):
+                            valid_files = True
+                            break
+                if valid_files:
+                    break
+
+            if not valid_files:
+                print(f'Deleting directory (no matching files): {directory.name}')
+                shutil.rmtree(directory)
             else:
-                print(f'Skipping non-empty directory: {directory.name}')
+                print(f'Skipping directory with matching files: {directory.name}')
+
+
 
 def main():
     arguments = get_args()
     source = get_directory(arguments)
-    move_files_to_subfolders(source)
-    clean_up(source)
+    file_mappings = move_files_to_subfolders(source)
+    clean_up(source, file_mappings)
 
 if __name__ == '__main__':
     main()
