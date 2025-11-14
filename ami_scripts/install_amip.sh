5cfd27b2a124dff704140a708af7c90ca3d04566
bturkus <benjaminturkus@nypl.org>
2025-11-14 18:09:06 -0500

    Update install_amip.sh

--- ami_scripts/install_amip.sh
+++ ami_scripts/install_amip.sh
@@ -310,7 +310,7 @@ maintain_homebrew
 install_cli_packages() {
     local packages=(
         git coreutils grep jq xmlstarlet tree wget trash
-        p7zip rsync rclone gnu-tar awscli ntfs-3g clamav
+        p7zip rsync rclone gnu-tar awscli clamav
         graphicsmagick ffmpeg mediainfo mpc flac sox exiftool mkvtoolnix mediaconch qcli
         bagit rbenv jenv pyenv openjdk@11
     )
